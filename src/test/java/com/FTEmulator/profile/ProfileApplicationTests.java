7c9910f709943226c3dbefd45f9672ab46131874
alexwebdev05 <alexwebdev05@proton.me>
2025-06-23 21:19:04 +0200

    template

---
+++ src/test/java/com/FTEmulator/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FTEmulator.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
