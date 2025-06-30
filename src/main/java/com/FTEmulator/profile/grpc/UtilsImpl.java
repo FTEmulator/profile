ca66bd771d28e8eeb0823749da211025346bd7e6
alexwebdev05 <alexwebdev05@proton.me>
2025-06-30 20:16:39 +0200

    enable profileTest

--- src/main/java/com/FTEmulator/profile/grpc/UtilsImpl.java
+++ src/main/java/com/FTEmulator/profile/grpc/UtilsImpl.java
@@ -1,22 +1,22 @@
 package com.FTEmulator.profile.grpc;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.grpc.server.service.GrpcService;
 
-// import com.FTEmulator.profile.entity.User;
+import com.FTEmulator.profile.entity.User;
 import com.FTEmulator.profile.grpc.UtilsOuterClass.ProfileStatusRequest;
 import com.FTEmulator.profile.grpc.UtilsOuterClass.ProfileStatusResponse;
-// import com.FTEmulator.profile.grpc.UtilsOuterClass.getTestRequest;
-// import com.FTEmulator.profile.grpc.UtilsOuterClass.getTestResponse;
-// import com.FTEmulator.profile.service.UserTestService;
+import com.FTEmulator.profile.grpc.UtilsOuterClass.getTestRequest;
+import com.FTEmulator.profile.grpc.UtilsOuterClass.getTestResponse;
+import com.FTEmulator.profile.service.UserTestService;
 
 import io.grpc.stub.StreamObserver;
 
 @GrpcService
 public class UtilsImpl extends UtilsGrpc.UtilsImplBase {
 
-    // @Autowired
-    // private UserTestService userTestService;
+    @Autowired
+    private UserTestService userTestService;
 
     // Status
     @Override
@@ -28,26 +28,26 @@ public class UtilsImpl extends UtilsGrpc.UtilsImplBase {
     }
 
     // Get user test
-    // @Override
-    // public void getUserTest(getTestRequest request, StreamObserver<getTestResponse> responseObserver) {
-    //     User userTest = userTestService.getTestByName("test");
+    @Override
+    public void getUserTest(getTestRequest request, StreamObserver<getTestResponse> responseObserver) {
+        User userTest = userTestService.getTestByName("test");
 
-    //     getTestResponse response;
-    //     if (userTest != null) {
-    //         response = getTestResponse.newBuilder()
-    //             .setId(userTest.getId().toString())
-    //             .setName(userTest.getName())
-    //             .setEmail(userTest.getEmail())
-    //             .setCountry(userTest.getCountry())
-    //             .setStatus("Ok")
-    //             .build();
-    //     } else {
-    //         response = getTestResponse.newBuilder()
-    //             .setStatus("No user Found")
-    //             .build();
-    //     }
+        getTestResponse response;
+        if (userTest != null) {
+            response = getTestResponse.newBuilder()
+                .setId(userTest.getId().toString())
+                .setName(userTest.getName())
+                .setEmail(userTest.getEmail())
+                .setCountry(userTest.getCountry())
+                .setStatus("Ok")
+                .build();
+        } else {
+            response = getTestResponse.newBuilder()
+                .setStatus("No user Found")
+                .build();
+        }
 
-    //     responseObserver.onNext(response);
-    //     responseObserver.onCompleted();
-    // }
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
--- src/main/java/com/FTEmulator/profile/service/UserTestService.java
+++ src/main/java/com/FTEmulator/profile/service/UserTestService.java
@@ -1,18 +1,18 @@
-// package com.FTEmulator.profile.service;
+package com.FTEmulator.profile.service;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import com.FTEmulator.profile.repository.UserTestRepository;
-// import com.FTEmulator.profile.entity.User;
+import com.FTEmulator.profile.repository.UserTestRepository;
+import com.FTEmulator.profile.entity.User;
 
-// @Service
-// public class UserTestService {
+@Service
+public class UserTestService {
     
-//     @Autowired
-//     private UserTestRepository UserTestRepository;
+    @Autowired
+    private UserTestRepository UserTestRepository;
 
-//     public User getTestByName(String name) {
-//         return UserTestRepository.findByName(name);
-//     }
-// }
+    public User getTestByName(String name) {
+        return UserTestRepository.findByName(name);
+    }
+}
